c40a01a6f2099586edf782b28b02784a3252b0a3
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2024-04-20 12:38:13 +0530

    Delete scripts directory

--- scripts/dotnet-publish.sh
+++
@@ -1,4 +0,0 @@
-cd ../src/Services
-dotnet publish ./Instagram.Services.User -c Release -o ./Instagram.Services.User/bin/Docker
-dotnet publish ./Instagram.Services.Post -c Release -o ./Instagram.Services.Post/bin/Docker
-dotnet publish ./Instagram.Services.Newsfeed -c Release -o ./Instagram.Services.Newsfeed/bin/Docker
